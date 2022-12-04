56cfe59a0b245324b9abe882afc9ca9980d04138
MosiurRrahman <mosiurrahman595@gmail.com>
2022-12-04 15:00:12 +0600

    dadadada

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import reactLogo from './assets/react.svg'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -25,7 +25,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React
       </p>
     </div>
   )
